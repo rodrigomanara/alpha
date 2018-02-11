9a98336a7344e2774ffe64b5b1fec467c4acce4d
LAPTOP-PLVVCJSK\Rodrigo <rodrigomanara@yahoo.co.uk>
2018-02-11 22:09:45 +0000

    add new methods

--- alpha.php
+++ alpha.php
@@ -4,9 +4,9 @@ namespace Alpha;
 
 class Alpha {
 
-    
     var $alpha = ["a", "e", "i", "o", "u"];
-    
+    var $set;
+
     /**
      * 
      * @param type $str
@@ -33,12 +33,17 @@ class Alpha {
      * @param type $str
      * @return int
      */
-    public function checkVowels($alpha) {
+    public function checkVowels($string) {
 
-        if (in_array($alpha, $this->alpha) && ctype_alpha($alpha)) {
-            return 1;
+
+        $count = 0;
+        foreach (str_split($string) as $alpha) {
+            if (in_array($alpha, $this->alpha) && ctype_alpha($alpha)) {
+                $count++;
+                $this->set('vowels', $alpha);
+            }
         }
-        return 0;
+        return $count;
     }
 
     /**
@@ -48,12 +53,18 @@ class Alpha {
      * @param type $str
      * @return int
      */
-    public function checkConsonates($alpha) {
+    public function checkConsonates($string) {
 
-        if (!in_array($alpha, $this->alpha) && ctype_alpha($alpha)) {
-            return 1;
+        
+        $count = 0;
+        foreach (str_split($string) as $alpha) {
+            if (!in_array($alpha, $this->alpha) && ctype_alpha($alpha)) {
+                $count++;
+                $this->set('consonantes', $alpha);
+            }
         }
-        return 0;
+        return $count;
+         
     }
 
     /**
@@ -63,12 +74,14 @@ class Alpha {
      */
     public function checkNumber($str) {
 
+        
         $string = str_split($str);
 
         $count = 0;
         foreach ($string as $letters) {
             if (is_numeric($letters)) {
                 $count++;
+                $this->set('numbers', $letters);
             }
         }
 
@@ -92,6 +105,82 @@ class Alpha {
         }
 
         return ($avarage / $counter) > 0 ? ceil(abs($avarage / $counter)) : 0;
+    }
+    /**
+     * 
+     * @param type $string
+     * @return int
+     */
+    public function noalpha($string){
+        
+        $count = 0;
+        foreach (str_split($string) as $alpha) {
+            if (!preg_match("/[a-zA-Z0-9]/i", $alpha) && ctype_alpha($alpha)) {
+                $count++;
+                $this->set('noalpha', $alpha);
+            }
+        }
+        return $count;
+    }
+    /**
+     * 
+     * @param type $string
+     * @return int
+     */
+    public function upper($string){
+        
+        $count = 0;
+        foreach (str_split($string) as $alpha) {
+            if (preg_match("/[A-Z]/", $alpha) && ctype_alpha($alpha)) {
+                $count++;
+                $this->set('upper', $alpha);
+            }
+        }
+        return $count;
+    }
+    /**
+     * 
+     * @param type $string
+     * @return int
+     */
+    public function lower($string){
+        
+        $count = 0;
+        foreach (str_split($string) as $alpha) {
+            if (preg_match("/[a-z]/", $alpha) && ctype_alpha($alpha)) {
+                $count++;
+                $this->set('lowers', $alpha);
+            }
+        }
+        return $count;
+    }
+
+    /**
+     * 
+     * @param type $key
+     * @param type $value
+     */
+    public function set($key, $value) {
+
+        if (isset($this->set[$key])) {
+
+            if (count($this->set[$key]) == 1)
+                unset($this->set[$key]);
+            $this->set[$key][] = $value;
+        }else {
+            $this->set[$key] = $value;
+        }
+    }
+
+    public function get($key) {
+        if (isset($this->set[$key])) {
+            return $this->set[$key];
+        }
+        return [];
+    }
+
+    public function getAll() {
+        return $this->set;
     }
 
 }
